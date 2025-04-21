1f8de122c145db661a81e7ea34db9b665f5fd06d
Adarash-mehra <infoaadarshmehra@gmail.com>
2025-04-21 21:37:14 +0530

    try to create csv file

--- List_users.py
+++ List_users.py
@@ -1,12 +1,23 @@
 import boto3
 import json
+import csv
 
 
 session = boto3.Session()
 client = session.client('iam')
 luser = client.list_users()
 
-print(luser['Users'][0]['UserName'])
-for i in luser['Users']:
-    print(i['UserId'])
-    print(i['UserName'])
+def list_users():
+    dictt = []
+    print(luser['Users'][0]['UserName'])
+    for i in luser['Users']:
+        dictt1 = i['UserId']
+        dictt2 = i['UserName']
+        dictt.append({'id': dictt1,'name': dictt2})
+
+        return dictt
+p = list_users()
+with open('list_users.csv','w') as file:
+    write = csv.DictWriter(file, fieldnames=['id','name'])
+    write.writeheader()
+    write.writerows(p)
